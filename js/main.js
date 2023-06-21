54dd6f9f2b2b5dd53bb64f40c8b3af3bc985d60f
Jacob Wassall-jamieson <jacobwassall-jamieson@Jacobs-MacBook-Pro.local>
2023-06-21 17:36:04 +0200

    image switcher finished

--- js/main.js
+++ js/main.js
@@ -40,13 +40,13 @@ class Switcher {
     constructor(app, data) {
         this.app = app; // Class app
         this.data = data;
-        this.yubtub = new Yubtub(this.app, data);
+        this.yubtub = new Yubtub(this.app, this.data, this.data[0]);
         this.cleaner = new Cleaner();
     }
 
-    switch(link) {
+    switch(id) {
         this.cleaner.clean("body");
-        this.yubtub = new Yubtub(this.app, this.data[link]);
+        this.yubtub = new Yubtub(this.app, this.data, this.data[id]);
     }
 
 }
@@ -69,10 +69,10 @@ class Yubtub {
     renderer;
     app;
 
-    constructor(app, data) {
+    constructor(app, data, dataOnload) {
         this.app = app;
         this.renderer = new Renderer();
-        this.main = new Main(this, data);
+        this.main = new Main(this, dataOnload);
         this.aside = new Aside(this, data);
 
     }
@@ -82,21 +82,77 @@ class Yubtub {
 class Main {
     video;
 
-    constructor(yubtub, data) {
+    constructor(yubtub, dataOnload) {
         this.yubtub = yubtub;
-        this.data = data;
+        this.dataOnload = dataOnload;
 
         this.htmlElement = document.createElement("main");
         this.htmlElement.classList.add("main");
 
         this.yubtub.renderer.render(document.querySelector("body"), this.htmlElement);
 
-        this.video = new Video();
+        this.videoSection = document.createElement("section");
+        this.videoSection.classList.add("video");
+
+        this.yubtub.renderer.render(this.htmlElement, this.videoSection);
+
+        this.topIcon = document.createElement("i");
+        this.topIcon.classList = "fa-solid fa-star video__star video__star--top";
+
+        this.yubtub.renderer.render(this.videoSection, this.topIcon);
+
+        this.infobox = document.createElement("ul");
+        this.infobox.classList.add("video__infoBoxs");
+
+        this.yubtub.renderer.render(this.videoSection, this.infobox);
+
+        this.infoboxFirst = document.createElement("li");
+        this.infoboxFirst.classList = "video__infoBox video__infoBox--first";
+
+        this.yubtub.renderer.render(this.infobox, this.infoboxFirst);
+
+        this.profile = document.createElement("figure");
+        this.profile.classList.add("video__profile");
+
+        this.yubtub.renderer.render(this.infoboxFirst, this.profile);
+
+        this.title = document.createElement("h2");
+        this.title.classList.add("video__title");
+        this.title.innerText = "hoi"
+
+        this.yubtub.renderer.render(this.infoboxFirst, this.title);
+
+        this.infoboxSecond = document.createElement("li");
+        this.infoboxSecond.classList = "video__infoBox video__infoBox--second";
+
+        this.yubtub.renderer.render(this.infobox, this.infoboxSecond);
+
+        this.star = document.createElement("i");
+        this.star.classList = "fa-solid fa-star video__star";
+
+        this.yubtub.renderer.render(this.infoboxSecond, this.star);
+
+        this.arrow = document.createElement("i");
+        this.arrow.classList = "fa-solid fa-arrow-right video__arrow";
+
+        this.yubtub.renderer.render(this.infoboxSecond, this.arrow);
+
+        this.video = new Video(dataOnload);
+
+        this.yubtub.renderer.render(this.videoSection, this.video.htmlElement);
     }
 }
 
 class Video {
+    dataOnload;
 
+    constructor(dataOnload) {
+        this.dataOnload = dataOnload;
+
+        this.htmlElement = document.createElement("video");
+        this.htmlElement.classList = "video__video";
+        this.htmlElement.src = "./video/" + dataOnload["video"];
+    }
 }
 
 class Comments {
@@ -139,13 +195,16 @@ class NextVideo {
             this.htmlElement = document.createElement("video");
             this.htmlElement.classList.add("featured__video");
             this.htmlElement.src = "./video/" + data[i].video;
-            this.htmlElement.onclick = this.videoClicked;
+            this.htmlElement.id = data[i].id;
+            this.htmlElement.onclick = () => {
+                this.videoClicked(data[i].id)
+            };
             this.aside.yubtub.renderer.render(this.aside.htmlElement, this.htmlElement);
         }
     }
 
-    videoClicked = () => {
-        this.aside.yubtub.app.switcher.switch(this.data.link);
+    videoClicked = (id) => {
+        this.aside.yubtub.app.switcher.switch(id);
     }
 
 }
